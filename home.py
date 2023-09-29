0ae49e65ac812ddf1bcc03bc27f5c869a8c7c3b3
DORIS RUSH-LOPEZ <info@bonnysites.com>
2023-09-29 13:38:10 -0400

    fixed markdown error

--- home.py
+++ home.py
@@ -20,12 +20,12 @@ st.set_page_config(
     page_icon=":cityscape:"
 )
 
-# hide_default_format = """
-#        <style>
-#        #MainMenu {visibility: hidden; }
-#        footer {visibility: hidden;}
-#        </style>
-#        """
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 
 # End snippet 
